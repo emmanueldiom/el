8625edee61a79abc12e530deffafa61cc935cdad
emmanueldiom <diomemmanuel01@gmail.com>
2025-02-14 23:44:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("messageButton").addEventListener("click", function() {
+    alert("vous êtes bien sur le site de Diomandé Emmanuel.  Nous vous remercions pour votre passage sur ce site!");
+});
